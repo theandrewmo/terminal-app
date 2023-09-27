0adbf4f220c46f136b471aff20374b053ad4c5b1
theandrewmo <106348010+theandrewmo@users.noreply.github.com>
2023-09-27 08:33:25 -0600

    Initial Commit

---
+++ terminal_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  terminal_app
+//
+//  Created by Andrew Morrison on 9/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ terminal_app/terminal_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  terminal_appApp.swift
+//  terminal_app
+//
+//  Created by Andrew Morrison on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct terminal_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
